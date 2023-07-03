9e53180d855aa2280d8c3d924bc978486b006023
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-07-03 12:31:18 +0100

    base commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('I Am Poor'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+          child: Image(image: AssetImage('lib/images/poorman.jpg'))),
+    ),
+  ));
+}
